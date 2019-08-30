de8d0c07f29cbe8de7677b39fd8298f9870d9995
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2019-08-31 01:13:56 +0200

    Created DB models

---
+++ database_setup.py
@@ -0,0 +1,38 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Channel(db.Model):
+    __tablename__ = "channel"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String())
+    link = db.Column(db.String())
+    atom = db.Column(db.String())
+    description = db.Column(db.String())
+    language = db.Column(db.String())
+    copyright = db.Column(db.String())
+    lastBuildDate = db.Column(db.String())
+    img_title = db.Column(db.String())
+    img_url = db.Column(db.String())
+    img_link = db.Column(db.String())
+
+
+
+class Item(db.Model):
+    __tablename__ = 'item'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String())
+    link = db.Column(db.String())
+    atom = db.Column(db.String())
+    description = db.Column(db.String())
+    creator = db.Column(db.String())
+    pubDate = db.Column(db.String())
+    category = db.Column(db.String())
+    media_content = db.Column(db.String())
+    media_credit = db.Column(db.String())
+    media_description = db.Column(db.String())
+    time = db.Column(db.String())
+    channel_id = db.Column(db.Integer, db.ForeignKey('channel.id'))
+    channel = db.relationship(Channel)
